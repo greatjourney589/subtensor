7d0c0bf92d8a7129d91ff6cd929e6ba1a7905174
greatjourney589 <wangkyle103@gmail.com>
2026-04-17 05:26:20 -0700

    refactor: simplify vec_to_hash using deref coercion

--- pallets/subtensor/src/subnets/registration.rs
+++ pallets/subtensor/src/subnets/registration.rs
@@ -212,10 +212,7 @@ impl<T: Config> Pallet<T> {
     }
 
     pub fn vec_to_hash(vec_hash: Vec<u8>) -> H256 {
-        let de_ref_hash = &vec_hash; // b: &Vec<u8>
-        let de_de_ref_hash: &[u8] = de_ref_hash; // c: &[u8]
-        let real_hash: H256 = H256::from_slice(de_de_ref_hash);
-        real_hash
+        H256::from_slice(&vec_hash)
     }
 
     fn get_immune_owner_hotkeys(netuid: NetUid, coldkey: &T::AccountId) -> Vec<T::AccountId> {
